0f063eb061e649bee2cc9e4e92f6c927c87014e7
Nguyen Tuan Anh <nguyentuananh300905@gmail.com>
2024-10-29 23:36:22 +0700

    commit choose movie

--- assets/js/buy-ticket-menu.js
+++ assets/js/buy-ticket-menu.js
@@ -2,11 +2,20 @@ const buyBtns = document.querySelectorAll('.js-buy-ticket');
 const closeBuyBtn = document.querySelector('.close-buy-ticket');
 const BuyBtnContainer = document.querySelector('.buy-ticket-wrapper');
 const BuyBtnContent = document.querySelector('.buy-ticket-content');
-
 for(const buyBtn of buyBtns){
     buyBtn.addEventListener('click', function (event) {
         event.preventDefault();
         BuyBtnContainer.classList.add("active");
+
+        const product = buyBtn.closest(".product");
+        const productImgSrc = product.getAttribute("data-img-src");
+        const productName = product.getAttribute("data-movieName");
+
+        const movie = {
+            movieName : productName,
+            movieImgSrc : productImgSrc
+        };
+        localStorage.setItem("movie",JSON.stringify(movie));
     });
 }
 
