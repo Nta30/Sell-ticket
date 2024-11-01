ce93e1753295f4197470c139995777d7a89ebd6c
Nguyen Tuan Anh <nguyentuananh300905@gmail.com>
2024-11-01 17:48:37 +0700

    add trailer

--- assets/js/main-trailer.js
+++ assets/js/main-trailer.js
@@ -1,21 +1,25 @@
 // trailer
-const watchTrailer = document.querySelector('.movie-btn-trailer'); //lay tat ca cac phan tu co class
+const watchTrailer = document.querySelectorAll('.movie-btn-trailer'); //lay tat ca cac phan tu co class
 const model = document.querySelector('.model');
 const modelclose = document.querySelector('.close-model');
 const trailerVideo = document.querySelector('.movie-trailer');
 // ham hien thi model trailer (them class open model )
 function showTrailer(){
-    model.classList.add('open');   
+    model.classList.add('open')   
     trailerVideo.play();
 }
 // ham an model trailer (go class open model )
 function closeTrailer(){
-    model.classList.remove('open');
+    model.classList.remove('open')  
     trailerVideo.currentTime = 0;
     trailerVideo.pause();
 }
 // lap qua tung the va nghe hanh vi click
-watchTrailer.addEventListener('click',showTrailer);
+for (const Trailer of watchTrailer){
+    Trailer.addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+    Trailer.addEventListener('click',showTrailer)// biet duoc minh dang click
+}
 // nghe hanh vi click vao btn close
 modelclose.addEventListener('click',closeTrailer);
-
